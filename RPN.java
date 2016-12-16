3a64f8fff388ba612f37fc94ccbc3c60259a7399
Nathalie Tate <nathalie.tate00@gmail.com>
2016-12-16 04:39:41 -0500

    fix #1

--- RPN.java
+++ RPN.java
@@ -6,17 +6,19 @@
  * LICENSE.txt for details.
  */
 import java.util.Scanner;
+import java.util.Stack;
 
 public class RPN
 {
 
-  public static double rpn(double a, double b, char c) throws Exception
+	//b and a are backwards so division makes sense from the stack
+  public static double rpn(double b, double a, char c) throws Exception
   {
     if(c!='=' && c!='+' && c!='-' && c!='*' && c!='/' && c!='%')
     {
       throw new Exception();
     }
-    if(c=='+')
+		else if(c=='+')
       return a+b;  
     else if(c=='=')
     {
@@ -30,7 +32,11 @@ public class RPN
     else if(c=='*')
       return a*b;
     else if(c=='/')
+		{
+			if(b==0)
+				System.exit(0);
       return a/b;
+		}
     else if(c=='%')
       return a%b;
     else
@@ -42,24 +48,29 @@ public class RPN
     System.out.println("RPN Calculator");
     System.out.println("Nathalie Tate");
     System.out.println("div by zero to quit");
-    double a, b;
-    String tmp;
-    char c;
+    double a;
+    String a_;
+		Stack<Double> operands = new Stack<Double>();
+		Stack<Character> operators = new Stack<Character>();
+		Scanner in = new Scanner(System.in);
     while(true)
     {
      try{
-      Scanner in = new Scanner(System.in);
-      a=in.nextDouble();
-      b=in.nextDouble();
-      tmp=in.next();
-      c=tmp.charAt(0);
-      if(b==0 && c=='/')
-        break;
-      System.out.println(rpn(a,b,c));  
-      System.out.println("_________________");
-     }catch(Exception e){
-       System.out.println("ERROR");
-       System.out.println("_________________");};
-    }
-  }
+			 a_=in.next();
+
+			 try{
+				 operands.push(Double.parseDouble(a_)); 
+			 }catch(Exception e)
+			 {
+				 operators.push(a_.charAt(0));
+				 operands.push(rpn(operands.pop(),operands.pop(),operators.pop()));
+				 System.out.println(operands.peek());
+				 System.out.println("_________________");
+			 }
+
+		 }catch(Exception e){
+			 System.out.println("ERROR");
+			 System.out.println("_________________");};
+		}
+	}
 }
